49f6f34e66a2a10bd4854d379b64e57c37ded82e
Scott <hartsa1993@gmail.com>
2019-12-13 00:38:47 +1030

    Changed movement to velocity, Added moving level script to move platforms, made platforms prefabs
    Changed movement script to adding velocity to rigid body
    Added MovingLevel.cs that moves platforms to the left and checks if they are off screen (currently moving them to the right of the screen)

---
+++ Assets/MovingLevel.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingLevel : MonoBehaviour
+{
+
+    List<GameObject> platforms;
+    float moveSpeed = 1.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        platforms = new List<GameObject>();
+        foreach (Transform t in transform)
+            platforms.Add(t.gameObject);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+        foreach(GameObject p in platforms)
+        {
+            if (p != null)
+            {
+                //Move left
+                p.transform.position += Vector3.left * moveSpeed * Time.deltaTime;
+                
+                //Deal if off screen
+                Bounds bounds = new Bounds(p.transform.position, Vector3.zero);
+                foreach (var renderer in p.GetComponentsInChildren<SpriteRenderer>())
+                    bounds.Encapsulate(renderer.bounds);
+                if (Camera.main.WorldToScreenPoint(bounds.center + Vector3.right * bounds.extents.x).x < 0)
+                {
+                    //Destroy(p.gameObject);
+                    p.transform.position = 
+                        new Vector3(Camera.main.ScreenToWorldPoint(new Vector3(Camera.main.pixelWidth, 0, 0)).x, p.transform.position.y, p.transform.position.z) //Move center to right side
+                        + Vector3.right * bounds.extents.x; //Offset to off screen
+                }
+            }
+        }
+    }
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -18,7 +18,7 @@ public class PlayerController : MonoBehaviour
     public JumpState jump = JumpState.Double;
     bool jumpInput = false;
     float jumpForce = 5;
-    float moveSpeed = 1;
+    float moveSpeed = 10;
     LayerMask groundMask;
     [SerializeField]
     Transform target;
@@ -34,6 +34,7 @@ public class PlayerController : MonoBehaviour
     {
         animator.SetBool("Running", true);
         groundMask = LayerMask.GetMask("Ground");
+        if (target == null) throw new System.Exception("target null");
     }
 
     private void Update()
@@ -46,27 +47,7 @@ public class PlayerController : MonoBehaviour
 
     private void FixedUpdate()
     {
-        //Move to target
-        if (target != null && Mathf.Abs(transform.position.x - target.position.x) > 0.5f)
-            transform.position += (new Vector3((target.position.x - rigidbody.position.x), 0, 0).normalized * moveSpeed * Time.fixedDeltaTime);
-        //Jump check
-        if (jumpInput)
-        {
-            if (Mathf.Abs(rigidbody.velocity.y) < 0.1 && jump == JumpState.Ground)
-            {
-                rigidbody.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
-                jump = JumpState.Jump;
-                animator.SetBool("Jumping", true);
-
-            }
-            else if (jump == JumpState.Jump)
-            {
-                rigidbody.velocity = new Vector2(rigidbody.velocity.x, 0); // reset velocity  
-                rigidbody.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
-                jump = JumpState.Double;
-            }
-            jumpInput = false;
-        }
+        Vector2 velocity = rigidbody.velocity;
         //Grounded check
         bool grounded = false;
         Vector3[] pos = new Vector3[]{
@@ -85,7 +66,30 @@ public class PlayerController : MonoBehaviour
             jump = JumpState.Jump;
             animator.SetBool("Jumping", true);
         }
+        //Move to target
+        if (grounded && Mathf.Abs(transform.position.x - target.position.x) > 0.5f)
+            velocity.x += Mathf.Sign(target.position.x - rigidbody.position.x) * moveSpeed * Time.fixedDeltaTime;
+        //transform.position += (new Vector3((target.position.x - rigidbody.position.x), 0, 0).normalized * moveSpeed * Time.fixedDeltaTime);
+        Mathf.Clamp(velocity.x, -2, 2);
+        rigidbody.velocity = velocity;
+        //Jump check
+        if (jumpInput)
+        {
+            if (grounded && jump == JumpState.Ground)
+            {
+                rigidbody.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+                jump = JumpState.Jump;
+                animator.SetBool("Jumping", true);
 
+            }
+            else if (jump == JumpState.Jump)
+            {
+                rigidbody.velocity  = new Vector2(rigidbody.velocity.x, 0); //Stop downward momentum to give nicer feeling jump
+                rigidbody.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+                jump = JumpState.Double;
+            }
+            jumpInput = false;
+        }
 
     }
     public void Coin()
