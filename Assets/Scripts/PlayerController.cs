9289329afa85852164e68540ac1c5cfa2213239e
Scott <hartsa1993@gmail.com>
2019-12-15 00:45:11 +1030

    Set up Android dev settings, added mouse as jump input, set scale on canvas

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -62,7 +62,7 @@ public class PlayerController : MonoBehaviour
 
     private void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space) || Input.GetMouseButtonDown(0))
         {
             jumpInput = true;
         }
